8d06eac611b6465c38b59f75b02e6f7a26b56459
burgokmen <burgokmen@gmail.com>
2023-10-05 14:44:56 +0300

    decimalToBinary done

--- src/com/brutech/entity/Main.java
+++ src/com/brutech/entity/Main.java
@@ -1,7 +1,6 @@
 package com.brutech.entity;
 
-import java.util.Iterator;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,10 +10,17 @@ public class Main {
         System.out.println(checkForPalindrome("I did, did I?"));
         System.out.println(checkForPalindrome("hello"));
         System.out.println(checkForPalindrome("Don't node"));
+        System.out.println("-------------------------");
+        System.out.println(decimalToBinary(24));
+        System.out.println(decimalToBinary(0));
+        System.out.println(decimalToBinary(2));
+        System.out.println(decimalToBinary(3));
+        System.out.println(decimalToBinary(9999));
+        System.out.println(decimalToBinary(-24));
 
     }
 
-    public static boolean checkForPalindrome(String text) {
+  /*  public static boolean checkForPalindrome(String text) {
         String textClean = text.toLowerCase().replaceAll("[^a-za]", "");
         Stack<String> strStack = new Stack<>();
       for(char c : textClean.toCharArray()){
@@ -29,5 +35,48 @@ public class Main {
     }
     //System.out.println(reversed);
         return reversed.toString().equals(textClean);
+    }*/
+
+        public static boolean checkForPalindrome(String text) {
+        String textClean = text.toLowerCase().replaceAll("[^a-za]", "");
+        Stack<String> strStack = new Stack<>();
+        for(char c : textClean.toCharArray()){
+            strStack.push(String.valueOf(c));
+        }
+        Queue<String> strQueue = new LinkedList<>();
+        //System.out.println(strStack);
+        while(!strStack.isEmpty()){
+            strQueue.add(strStack.pop());
+        }
+
+
+        //System.out.println(strQueue);
+
+        return true;
+
+         }
+
+      /*   public static Map<Integer, String> decimalToBinary(int num){
+             Map<Integer, String> binary = new TreeMap<>(Comparator.reverseOrder());
+             Integer [] keys = [9,8,7,6,5,4,3,2,1,0]
+             switch ()
+
+             return binary;
+         }*/
+
+    public static String decimalToBinary(int decimal) {
+        decimal = Math.abs(decimal);
+
+        if (decimal == 0) {
+            return "0";
+        }
+
+        StringBuilder binary = new StringBuilder();
+        while (decimal > 0) {
+            int remainder = decimal % 2;
+            binary.insert(0, remainder);
+            decimal = decimal / 2;
+        }
+        return binary.toString();
     }
 }
